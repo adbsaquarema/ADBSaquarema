71a857b79272f8e416e3869353d48e4005a5df7a
Gabriel Levindo <69587938+gabrielslevindo@users.noreply.github.com>
2023-09-22 20:20:52 -0300

    comit a Forgot Password Screen in operation

---
+++ app/src/main/java/com/example/adbsaquarema/UI/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.adbsaquarema.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.adbsaquarema.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
